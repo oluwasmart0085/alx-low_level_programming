3f1d0e04f783845d75b9bcf50179b28f7ac35ef9
oluwasmart0085 <lebioluwasoji@gmail.com>
2022-07-18 14:56:36 -0700

    update

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -21,10 +21,10 @@ void simple_print_buffer(char *buffer, unsigned int size)
 		}
 		if (!(i % 10) && i)
 		{
-			printf("\n")
+			printf("\n");
 		}
 		printf("0x%02x", buffer[i]);
-		i++
+		i++;
 	}
 	printf("\n");
 }
