ed91d3ae7e4b57e86f696d4dcb016ba42abc6e2b
oluwasmart0085 <lebioluwasoji@gmail.com>
2022-08-16 10:09:12 -0700

    102-free_listint_safe

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Free a listint_t list and set the head to NULL
+ * @h: The list
+ *
+ * Return: The number of nodes in the list that were freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listint_t *temp;
+
+	if (!h || !(*h))
+		return (0);
+
+	while (*h)
+	{
+		size++;
+
+		if (*h <= (*h)->next)
+		{
+			free(*h);
+			*h = NULL;
+			break;
+		}
+
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+	}
+
+	*h = NULL;
+
+	return (size);
+}
