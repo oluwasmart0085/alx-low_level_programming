800b3893063414301c3ebf15d210279717242037
oluwasmart0085 <lebioluwasoji@gmail.com>
2022-08-22 12:38:41 -0700

    main h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+void args_error(void);
+void file_from_error(char *file);
+void file_to_error(char *file);
+void close_error(int fd);
+void print_elf(void);
+
+int _putchar(char c);
+int _strlen(char *s);
+
+#endif
