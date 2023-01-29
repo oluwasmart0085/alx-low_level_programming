9eb56a90145b01c869306259cb2bbef41b7aaef3
oluwasmart <lebioluwasoji@gmail.com>
2023-01-29 12:12:20 -0800

    search algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void printArray(int *array, size_t low, size_t high);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+#endif
