f5370815198defc080fa19e81302b83a85bca8b1
oluwasmart <lebioluwasoji@gmail.com>
2023-01-29 12:02:30 -0800

    serach algorithms

--- 0x0A-argc_argv/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-int is_positive_number(char *number);
-int number_of_coins(int cents);
-
-#endif
--- 0x0E-structures_typedef/README.md
+++
@@ -1,1 +0,0 @@
- a new type struct dog with the following elements
